e711512077515c6a2f9072330908df88d1248958
weihanli <weihanli@outlook.com>
2019-06-11 22:49:43 +0800

    add missing file

---
+++ src/WeihanLi.EntityFramework/EFRepositoryGeneratorOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeihanLi.EntityFramework
+{
+    public class EFRepositoryGeneratorOptions
+    {
+        public bool GenerateInterface { get; set; } = true;
+
+        /// <summary>
+        /// RepositoryNameResolver
+        /// </summary>
+        public Func<string, string> RepositoryNameResolver { get; set; }
+            = entityName => $"{entityName}Repository";
+    }
+}
