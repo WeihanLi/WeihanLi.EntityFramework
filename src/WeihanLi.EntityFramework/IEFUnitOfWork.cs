637b00a0113618e8e993b44268f1d94d391302c0
weihanli <weihanli@outlook.com>
2020-02-26 01:19:28 +0800

    update QueryBuilder set default predict null add EFUnitOfWork

---
+++ src/WeihanLi.EntityFramework/IEFUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeihanLi.EntityFramework
+{
+    public interface IEFUnitOfWork<TDbContext> where TDbContext : DbContext
+    {
+        DbSet<TEntity> DbSet<TEntity>() where TEntity : class;
+
+        int Commit();
+
+        Task<int> CommitAsync(CancellationToken cancellationToken = default);
+    }
+}
