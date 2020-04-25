0707d5efda2816fefa44897484fc7e1a7ee88d63
weihanli <weihanli@outlook.com>
2020-04-25 18:07:04 +0800

    add AuditDbContextInterceptor

---
+++ src/WeihanLi.EntityFramework/Audit/AuditDbContextInterceptor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WeihanLi.Common.Aspect;
+
+namespace WeihanLi.EntityFramework.Audit
+{
+    public class AuditDbContextInterceptor : IInterceptor
+    {
+        public async Task Invoke(IInvocation invocation, Func<Task> next)
+        {
+            if (invocation.ProxyTarget is DbContext dbContext && AuditConfig.AuditConfigOptions.AuditEnabled)
+            {
+                var auditEntries = new List<AuditEntry>(16);
+                foreach (var entityEntry in dbContext.ChangeTracker.Entries())
+                {
+                    if (entityEntry.State == EntityState.Detached || entityEntry.State == EntityState.Unchanged)
+                    {
+                        continue;
+                    }
+                    if (AuditConfig.AuditConfigOptions.EntityFilters
+                        .Any(entityFilter =>
+                            entityFilter.Invoke(entityEntry) == false))
+                    {
+                        continue;
+                    }
+                    auditEntries.Add(new AuditEntry(entityEntry));
+                }
+                await next();
+                //
+                if (auditEntries.Count > 0)
+                {
+                    foreach (var auditEntry in auditEntries)
+                    {
+                        // update TemporaryProperties
+                        if (auditEntry.TemporaryProperties != null && auditEntry.TemporaryProperties.Count > 0)
+                        {
+                            foreach (var temporaryProperty in auditEntry.TemporaryProperties)
+                            {
+                                var colName = temporaryProperty.Metadata.GetColumnName();
+                                if (temporaryProperty.Metadata.IsPrimaryKey())
+                                {
+                                    auditEntry.KeyValues[colName] = temporaryProperty.CurrentValue;
+                                }
+
+                                switch (auditEntry.OperationType)
+                                {
+                                    case OperationType.Add:
+                                        auditEntry.NewValues[colName] = temporaryProperty.CurrentValue;
+                                        break;
+
+                                    case OperationType.Delete:
+                                        auditEntry.OriginalValues[colName] = temporaryProperty.OriginalValue;
+                                        break;
+
+                                    case OperationType.Update:
+                                        auditEntry.OriginalValues[colName] = temporaryProperty.OriginalValue;
+                                        auditEntry.NewValues[colName] = temporaryProperty.CurrentValue;
+                                        break;
+                                }
+                            }
+                            // set to null
+                            auditEntry.TemporaryProperties = null;
+                        }
+
+                        // apply enricher
+                        foreach (var enricher in AuditConfig.AuditConfigOptions.Enrichers)
+                        {
+                            enricher.Enrich(auditEntry);
+                        }
+
+                        auditEntry.UpdatedAt = DateTimeOffset.UtcNow;
+                        auditEntry.UpdatedBy = AuditConfig.AuditConfigOptions.UserIdProvider
+                            ?.GetUserId();
+                    }
+
+                    await Task.WhenAll(
+                            AuditConfig.AuditConfigOptions.Stores
+                            .Select(store => store.Save(auditEntries))
+                        );
+                }
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
