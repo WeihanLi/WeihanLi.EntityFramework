c61d05cb8b7f036791df49bbf4a65a7f01f9027d
weihanli <weihanli@outlook.com>
2021-02-02 23:47:02 +0800

    add AuditInterceptor

--- samples/WeihanLi.EntityFramework.Sample/Program.cs
+++ samples/WeihanLi.EntityFramework.Sample/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WeihanLi.Common;
-using WeihanLi.Common.Aspect;
 using WeihanLi.Common.Data;
 using WeihanLi.Common.Helpers;
 using WeihanLi.Common.Services;
@@ -28,16 +27,28 @@ namespace WeihanLi.EntityFramework.Sample
             loggerFactory.AddLog4Net();
 
             var services = new ServiceCollection();
-            services.AddProxyDbContext<TestDbContext>(options =>
+            services.AddDbContext<TestDbContext>(options =>
             {
                 options
                     .UseLoggerFactory(loggerFactory)
                     //.EnableDetailedErrors()
                     //.EnableSensitiveDataLogging()
                     //.UseInMemoryDatabase("Tests")
-                    .UseSqlServer(DbConnectionString)
+                    .UseSqlite("Data Source=Test.db")
+                    .AddInterceptors(new AuditInterceptor())
+                    //.UseSqlServer(DbConnectionString)
                     ;
             });
+            //services.AddProxyDbContext<TestDbContext>(options =>
+            //{
+            //    options
+            //        .UseLoggerFactory(loggerFactory)
+            //        //.EnableDetailedErrors()
+            //        //.EnableSensitiveDataLogging()
+            //        //.UseInMemoryDatabase("Tests")
+            //        .UseSqlServer(DbConnectionString)
+            //        ;
+            //});
 
             //services.AddProxyDbContextPool<TestDbContext>(options =>
             //{
@@ -51,21 +62,21 @@ namespace WeihanLi.EntityFramework.Sample
             //        ;
             //});
             services.AddEFRepository();
-            services.AddFluentAspects(options =>
-            {
-                //options.InterceptMethod<DbContext>(m =>
-                //        m.Name == nameof(DbContext.SaveChanges)
-                //        || m.Name == nameof(DbContext.SaveChangesAsync))
-                //    .With<AuditDbContextInterceptor>();
+            //services.AddFluentAspects(options =>
+            //{
+            //    //options.InterceptMethod<DbContext>(m =>
+            //    //        m.Name == nameof(DbContext.SaveChanges)
+            //    //        || m.Name == nameof(DbContext.SaveChangesAsync))
+            //    //    .With<AuditDbContextInterceptor>();
 
-                //为所有 DbContext 注册审计拦截器
-                //options.InterceptDbContextSaveWithAudit();
+            //    //为所有 DbContext 注册审计拦截器
+            //    //options.InterceptDbContextSaveWithAudit();
 
-                //options.InterceptDbContextSave<TestDbContext>()
-                //    .With<AuditDbContextInterceptor>();
+            //    //options.InterceptDbContextSave<TestDbContext>()
+            //    //    .With<AuditDbContextInterceptor>();
 
-                options.InterceptDbContextSaveWithAudit<TestDbContext>();
-            });
+            //    options.InterceptDbContextSaveWithAudit<TestDbContext>();
+            //});
             DependencyResolver.SetDependencyResolver(services);
 
             AutoAuditTest();
@@ -92,12 +103,12 @@ namespace WeihanLi.EntityFramework.Sample
             {
                 var path = Path.Combine(Directory.GetCurrentDirectory(), _fileName);
 
-                using (var fileStream = File.Exists(path)
+                await using var fileStream = File.Exists(path)
                     ? new FileStream(path, FileMode.Append)
-                    : File.Create(path)
-                    )
+                    : File.Create(path);
+                foreach (var entry in auditEntries)
                 {
-                    await fileStream.WriteAsync(auditEntries.ToJson().GetBytes());
+                    await fileStream.WriteAsync(entry.ToJson().GetBytes());
                 }
             }
         }
@@ -159,14 +170,14 @@ namespace WeihanLi.EntityFramework.Sample
                 dbContext.TestEntities.Add(testEntity2);
                 dbContext.SaveChanges();
             });
-            DependencyResolver.TryInvoke<TestDbContext>(dbContext =>
+            DependencyResolver.TryInvokeAsync<TestDbContext>(async dbContext =>
             {
                 dbContext.Remove(new TestEntity()
                 {
                     Id = 2
                 });
-                dbContext.SaveChanges();
-            });
+                await dbContext.SaveChangesAsync();
+            }).Wait();
             // disable audit
             AuditConfig.DisableAudit();
             // enable audit
---
+++ src/WeihanLi.EntityFramework/Audit/AuditInterceptor.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using WeihanLi.Common.Models;
+
+namespace WeihanLi.EntityFramework.Audit
+{
+    public class AuditInterceptor : SaveChangesInterceptor
+    {
+        protected List<AuditEntry>? AuditEntries { get; set; }
+
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            PreSaveChanges(eventData.Context);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            PreSaveChanges(eventData.Context);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        protected virtual void PreSaveChanges(DbContext dbContext)
+        {
+            if (AuditConfig.AuditConfigOptions.AuditEnabled && AuditConfig.AuditConfigOptions.Stores.Count > 0)
+            {
+                if (AuditEntries is null)
+                {
+                    AuditEntries = new List<AuditEntry>();
+                }
+                else
+                {
+                    AuditEntries.Clear();
+                }
+
+                foreach (var entityEntry in dbContext.ChangeTracker.Entries())
+                {
+                    if (entityEntry.State == EntityState.Detached || entityEntry.State == EntityState.Unchanged)
+                    {
+                        continue;
+                    }
+                    if (AuditConfig.AuditConfigOptions.EntityFilters.Any(entityFilter =>
+                        entityFilter.Invoke(entityEntry) == false))
+                    {
+                        continue;
+                    }
+                    AuditEntries.Add(new InternalAuditEntry(entityEntry));
+                }
+            }
+        }
+
+        protected virtual async Task PostSaveChanges()
+        {
+            if (null != AuditEntries && AuditEntries.Count > 0)
+            {
+                foreach (var entry in AuditEntries)
+                {
+                    if (entry is InternalAuditEntry auditEntry)
+                    {
+                        // update TemporaryProperties
+                        if (auditEntry.TemporaryProperties != null && auditEntry.TemporaryProperties.Count > 0)
+                        {
+                            foreach (var temporaryProperty in auditEntry.TemporaryProperties)
+                            {
+                                var colName = temporaryProperty.GetColumnName();
+                                if (temporaryProperty.Metadata.IsPrimaryKey())
+                                {
+                                    auditEntry.KeyValues[colName] = temporaryProperty.CurrentValue;
+                                }
+
+                                switch (auditEntry.OperationType)
+                                {
+                                    case DataOperationType.Add:
+                                        auditEntry.NewValues![colName] = temporaryProperty.CurrentValue;
+                                        break;
+
+                                    case DataOperationType.Delete:
+                                        auditEntry.OriginalValues![colName] = temporaryProperty.OriginalValue;
+                                        break;
+
+                                    case DataOperationType.Update:
+                                        auditEntry.OriginalValues![colName] = temporaryProperty.OriginalValue;
+                                        auditEntry.NewValues![colName] = temporaryProperty.CurrentValue;
+                                        break;
+                                }
+                            }
+                            // set to null
+                            auditEntry.TemporaryProperties = null;
+                        }
+                    }
+
+                    // apply enricher
+                    foreach (var enricher in AuditConfig.AuditConfigOptions.Enrichers)
+                    {
+                        enricher.Enrich(entry);
+                    }
+
+                    entry.UpdatedAt = DateTimeOffset.UtcNow;
+                    entry.UpdatedBy = AuditConfig.AuditConfigOptions.UserIdProvider
+                        ?.GetUserId();
+                }
+
+                await Task.WhenAll(
+                        AuditConfig.AuditConfigOptions.Stores
+                        .Select(store => store.Save(AuditEntries))
+                    );
+            }
+        }
+
+        public override int SavedChanges(SaveChangesCompletedEventData eventData, int result)
+        {
+            PostSaveChanges().Wait();
+            return base.SavedChanges(eventData, result);
+        }
+
+        public override async ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            await PostSaveChanges();
+            return await base.SavedChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
